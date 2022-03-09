df046856729170896dbd00a9fb6bda625203804a
ones1kk <kwsic@naver.com>
2022-03-09 23:35:09 +0900

    ✨ Add : Error page supported by springboot

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -15,6 +15,11 @@ public class ServletExController {
         throw new RuntimeException("Exception");
     }
 
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 error");
+    }
+
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 error");
